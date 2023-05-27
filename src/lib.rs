e1e4a00c4af1364368f7a70c3ba9eebd920ea4b3
c-l-b-11 <caius.blumenthal@web.de>
2023-05-27 13:47:27 +0200

    inital commit off the progress already made offline

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 mod lexer;
+//mod parser;
 
 // Type definition for the Result that is being used by the parser. You may change it to anything
 // you want
@@ -7,6 +8,7 @@ pub type ParseResult = Result<(), String>;
 pub use lexer::C1Lexer;
 pub use lexer::C1Token;
 
+
 // You will need a re-export of your C1Parser definition. Here is an example:
-// mod parser;
-// pub use parser::C1Parser;
+ mod parser;
+pub use parser::C1Parser;
